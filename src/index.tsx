a325d6d26c4c29737a983e5ad22481edb07efb34
padyabsaeed <padyabsaeed90@gmail.com>
2022-05-16 09:12:03 +0300

    manage baseurl

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <LanguageProvider>
         <AppProvider>
           <App />
